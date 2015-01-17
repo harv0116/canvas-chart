459c3498a3aa11ef7b5a686ea30b3e794eb03a3f
harv0116 <harv0116@algonquinlive.com>
2015-01-16 21:58:17 -0500

    update files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
 // JavaScript Document
+
+
+$(document).ready(function(){
+	$.ajax({
+		url: "js/cheese.js",
+		dataType: "json",
+		}).done( gotData ).fail( badStuff );
+});
+
+function gotData( data ){
+console.log("Made it here!");	
+var percentSales = [];
+var labelProduct = [];
+var colorSegment = [];	
+// first get the data figured out
+for (i=0;i<data.segments.length;i++) {	
+	percentSales[i] = (data.segments[i].value / 100);
+	labelProduct[i] = data.segments[i].label;
+	colorSegment[i] = data.segments[i].color;
+
+	console.log(percentSales[i], labelProduct[i], colorSegment[i]);
+}
+
+
+// second prepare the first graph
+
+// third prepare the second graph
+}
+
+function badStuff(  jqxhr, status, err){
+	console.log("Oh Oh Broken!");
+}
